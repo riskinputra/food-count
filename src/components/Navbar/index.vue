78145d4d0268107a900869069e8960f3ea38b940
riskinputra <riski.n.putra@gmail.com>
2020-08-14 20:53:54 +0700

    add globalsetup

---
+++ src/components/Navbar/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="navbar" class="navbar">
+    <div class="container container-fluid">
+      <div>
+        <img src="@/assets/logo-white.png" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Navbar"
+};
+</script>
+
+<style lang="scss" scoped>
+@import "./styles.scss";
+</style>
